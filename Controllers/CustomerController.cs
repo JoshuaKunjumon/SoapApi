a92db46d19c28a06284a06c4798e1d1c3622d63e
Kunjumon, Abhilash <Abhilash_Kunjumon@oxy.com>
2024-07-03 10:11:53 -0500

    feat():adding more soap endpoints

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceReference1;
+
+namespace SoapApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController() { }
+
+        [HttpGet]
+        public async Task<Author> GetAuthors()
+        {
+            var service = new CustomerServiceClient(CustomerServiceClient.EndpointConfiguration.BasicHttpBinding_ICustomerService);
+            return await service.GetCustomersAsync();
+        }
+
+        [HttpGet]
+        public async Task<Author> GetAuthors2()
+        {
+            var service = new CustomerServiceClient(CustomerServiceClient.EndpointConfiguration.BasicHttpBinding_ICustomerService);
+            return await service.GetCustomersAsync();
+        }
+
+        [HttpGet]
+        public async Task<Author> GetAuthors3()
+        {
+            var service = new CustomerServiceClient(CustomerServiceClient.EndpointConfiguration.BasicHttpBinding_ICustomerService);
+            return await service.GetCustomersAsync();
+        }
+
+    }
+}
